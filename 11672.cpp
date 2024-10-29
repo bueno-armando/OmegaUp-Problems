c67e84eb366ecca5dab247641cc9aa89101a881e
bueno-armando <armandoseb.bueno@gmail.com>
2024-10-29 14:46:48 -0600

    ADD: 11672.cpp, .gitignore

---
+++ 11672.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+unsigned int arr[100000];
+unsigned int arr_size;
+unsigned int nof_queries;
+char type;
+unsigned int x, y;
+
+int main(){
+    cin>>arr_size;
+    cin>>nof_queries;
+
+    for(int i=0 ; i<arr_size ; i++)
+        cin>>arr[i];
+    for (int i=0 ; i<nof_queries ; i++){
+        cin>>type>>x>>y;
+        if(type == 'C'){
+            auto[minVal, maxVal] = minmax_element(arr+x-1, arr+y);
+            cout<<*minVal<<' '<<*maxVal<<endl;
+        }
+        else if(type == 'A')
+            arr[x-1] = y;
+    }
+}
